bf8d26dbcc4eeaa2290c472598dab9d2b572710e
Stas512 <stas512@example.com>
2026-01-22 10:02:46 +0300

    fix: base './' + clean CSS

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/front/',  // ← Обязательно для repo "front"
+  base: './',  // ← Обязательно для repo "front"
 })
